62de33454a1b1f9126f83a9c5a6a40708a613a1f
Jorge Loyo <jorgenayati@gmail.com>
2025-11-02 11:40:08 -0300

    feat: agregar configuración de API de Google Calendar y actualizar variables de entorno

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   googleCalendar: {
-    clientId: '178323827960-6uk0huvpg2rb18ki9d0v207p6noqrlaa.apps.googleusercontent.com',
-    apiKey: 'AIzaSyAGNbbWUEiZ_Gfre6HpTqY6_VItOuOfwV0'
+    clientId: process.env['GOOGLE_CLIENT_ID'] || '',
+    apiKey: process.env['GOOGLE_API_KEY'] || ''
   }
 };
